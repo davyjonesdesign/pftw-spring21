f16f1dc35df3ed102f12888fc0504ad12fc4cf5f
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2021-01-26 15:50:18 -0600

    Updated excercise 2.1

---
+++ wk2/excercises/2.1/array.js
@@ -0,0 +1,11 @@
+const movie = window.prompt("What is your favorite movie?");
+const movieArr = [
+  "White Christmas",
+  "The Ghost of Mrs. Miur",
+  "Arsenic And Old Lace",
+  "The Thin Man",
+  "Charade",
+];
+movieArr.push(movie);
+console.log(movieArr);
+console.log(movieArr.toString());
