0d27c3567a7cbc23ffe2f99da1d2378497652745
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2021-02-06 13:35:32 -0600

    changed style in CSS and added restart to end of game confirms

--- wk3/assignment-quiz/quiz.js
+++ wk3/assignment-quiz/quiz.js
@@ -33,23 +33,28 @@ let statements = [
   },
   {
     question:
-      "It cannot be seen, cannot be felt,\n\nCannot be heard, cannot be smelt.\n\nIt lies behind stars and under hills,\n\nAnd empty holes it fills.\n\nIt comes first and follows after,\n\nEnds life, kills laughter.",
+      "It cannot be seen, cannot be felt,\n\nCannot be heard, cannot be smelt.\n\nIt lies behind stars and under hills,\n\nAnd empty holes it fills. It comes first and follows after,\n\nEnds life, kills laughter.",
     answer: "dark",
   },
 ];
 function next() {
   if (statements.length < 1) {
-    alert("you won");
-    return;
+  var r = confirm("Huzzah! The ring is destroyed and Middle Earth is saved!\n\nWould you like to play again?");
+  if (r == true) {
+    location.reload();
+  return false;
+  } 
+  return
   }
 
   // lose the game after 5 wrong answers
   if (numWrong > 4) {
-    alert(
-      "Sorry, but you have reached your limit of 5 wrong answers. You lose!"
-    );
+    let r = confirm("Unfortunately, you have fallen into a deep chasm.\n\n Play again to slay the Balrog and save Middle Earth.");
+    if (r == true) {
+    location.reload();
+    return false;
+    } 
     return;
-    response = "";
   }
   const randomIndex = Math.ceil(Math.random() * statements.length - 1);
   return statements[randomIndex];
@@ -63,12 +68,12 @@ function checkQuestion() {
       return currentQuestion.answer != statementObj.answer;
     });
     // this is the correct condition
-    response = "correct! next question";
-    responseColor = "green";
+    response = "Correct! Click here to answer next question.";
+    responseColor = "yellow";
     numRight++;
   } else {
     // this is the wrong condition
-    response = "oops, that wasn't quite right. Try another!";
+    response = "Oops, that wasn't quite right. Click here and try another!";
     responseColor = "red";
     numWrong++;
   }
@@ -92,27 +97,29 @@ function setup() {
   bg = loadImage("LOTR-background.jpg");
   createCanvas(windowWidth, windowHeight);
   heading = createElement("h1", ["LOTR Riddle Quiz"]);
-  heading.position(200, 75);
+  heading.position(100, 25);
   questionInput = createInput("");
   questionInput.size(550);
-  questionInput.position(200, 600);
+  questionInput.position(100, 375);
   questionInput.placeholder = "new text for email";
   submitAnswerButton = createButton("submit answer");
-  submitAnswerButton.size(200);
+  submitAnswerButton.size(250);
   submitAnswerButton.mousePressed(checkQuestion);
-  submitAnswerButton.position(200, 700);
+  submitAnswerButton.position(100, 475);
   restartButton = createButton("restart");
-  restartButton.size(200);
+  restartButton.size(250);
   restartButton.mousePressed(restartQuiz);
-  restartButton.position(425, 700);
+  restartButton.position(400, 475);
 }
 function draw() {
   background(bg);
   background("rgba(25, 25, 0, 0.85)");
   fill("white");
-  textSize(24);
-  text(message, 200, 200);
-  fill("#ffe365");
-  text(response, 200, 550);
-  text("You have only " + [5 - numWrong] + " chances remaining.", 200, 825);
+  textSize(20);
+  text(message, 100, 125);
+  fill(responseColor);
+  text(response, 125, 415);
+  fill("#fff");
+  text(numRight + " / 6 correct", 100, 575);
+  text([5 - numWrong] + " chances remaining", 460, 575);
 }
