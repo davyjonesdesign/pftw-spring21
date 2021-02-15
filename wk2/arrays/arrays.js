e5ba50499a39abb3901e947cba5039d1531522d3
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2021-02-15 15:10:48 -0600

    added wk5, ex 1

--- wk2/arrays/arrays.js
+++ wk2/arrays/arrays.js
@@ -1,23 +1,23 @@
 // variable but has multiple slots
-const myArr =['bob', 'betty', 'sue'];
+const myArr = ["bob", "betty", "sue"];
 const myOtherArray = new Array();
-myOtherArray.push('star wars'); // add items to end
-myOtherArray.push('raiders of the lost ark');
+myOtherArray.push("star wars"); // add items to end
+myOtherArray.push("raiders of the lost ark");
 // myArr.pop(); // removes last item
 // myArr.shift(); // remove first item
-myArr.unshift('james'); // add item to the beginning of the array
+myArr.unshift("james"); // add item to the beginning of the array
 
 console.log(myArr);
 console.log(myOtherArray);
 console.log(myOtherArray[0]); // read single item
 console.log(myArr[1]);
-console.log(myArr.indexOf('betty'));
-const bettyIdx = myArr.indexOf('betty');
+console.log(myArr.indexOf("betty"));
+const bettyIdx = myArr.indexOf("betty");
 console.log(bettyIdx); // if not found -1, if found real index
 if (bettyIdx > -1) {
-    console.log(myArr[bettyIdx]);
+  console.log(myArr[bettyIdx]);
 }
-myArr.splice(bettyIdx, 0, 'robert');
+myArr.splice(bettyIdx, 0, "robert");
 console.log(myArr);
 console.log(myArr.length);
 // Math.random() for random 0 and 1
@@ -25,3 +25,15 @@ console.log(myArr.length);
 // myArr.length - 1 to get last index number of an array
 const randomNumber = Math.round(Math.random() * (myArr.length - 1));
 console.log(myArr[randomNumber]);
+
+let greekMythicalCharacters = [
+  "Athena",
+  "Zeus",
+  "Hermes",
+  "Hercules",
+  "Hera",
+  "that one guy",
+];
+greekMythicalCharacters.pop();
+greekMythicalCharacters.push("Hades");
+console.log(greekMythicalCharacters);
--- wk3/loops/loops.js
+++ wk3/loops/loops.js
@@ -3,29 +3,36 @@
 //     console.log('this is i', i)
 // }
 
+// // for loop with an array
+// const myMovies = ['star wars', 'empire strikes back', 'return of the jedi', 'raiders of the lost ark', 'last crusade'];
+// for(let j = 0; j < myMovies.length; j++) {
+//     // myMovies.pop();
+//     console.log('i like', myMovies[j]);
+// }
+
 // for loop with an array
-const myMovies = ['star wars', 'empire strikes back', 'return of the jedi', 'raiders of the lost ark', 'last crusade'];
-for(let j = 0; j < myMovies.length; j++) {
-    // myMovies.pop();
-    console.log('i like', myMovies[j]);
+const myPuppers = ["Lucky", "Lucy", "Limda", "Lacey", "Carl"];
+for (let j = 0; j < myPuppers.length; j++) {
+  // myPuppers.pop();
+  console.log("My dog", myPuppers[j], "is a really good puppy.");
 }
 
 // while loop
-let myVar = false;
-while (myVar === false) {
-    console.log('this will happen once');
-    myVar = true;
-}
+// let myVar = false;
+// while (myVar === false) {
+//     console.log('this will happen once');
+//     myVar = true;
+// }
 
 // while loop removing items in array ***
-const myNums = [1, 2, 3, 4, 5, 6];
-while (myNums.length > 0) {
-    console.log('removing ' + myNums[myNums.length - 1]);
-    myNums.pop();
-}
-console.log(myNums);
-while (myNums.length < 6) {
-    myNums = [1, 2, 3, 4, 5, 6]
-    console.log('adding ' + myNums[myNums.length++]);
-}
-console.log(myNums);
+// const myNums = [1, 2, 3, 4, 5, 6];
+// while (myNums.length > 0) {
+//     console.log('removing ' + myNums[myNums.length - 1]);
+//     myNums.pop();
+// }
+// console.log(myNums);
+// while (myNums.length < 6) {
+//     myNums = [1, 2, 3, 4, 5, 6]
+//     console.log('adding ' + myNums[myNums.length++]);
+// }
+// console.log(myNums);
