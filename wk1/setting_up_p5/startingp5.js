7f490cd4793e5c2d0c1e3fc091843f1f17e40b06
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2021-02-24 17:26:15 -0600

    added wk6

---
+++ wk1/setting_up_p5/startingp5.js
@@ -0,0 +1,7 @@
+function setup() {
+    createCanvas(400, 400);
+}
+function draw() {
+    background(220);
+    ellipse(200,200,80,80)
+}
---
+++ wk6/assignment/main.js
@@ -0,0 +1,26 @@
+new Vue({
+  el: '#llamaFacts',
+  data: {
+    titleText: 'Llama Facts',
+    llamaCamal:
+      "Fossil footprints found in California indicate that llamas, relatives of camels, originated in North America. It is believed that the animals that moved north and crossed the Bering land bridge evolved into camels, while the ones that migrated to the south became the 'lama' family.",
+    llamaGroups:
+      'Llamas can be divided in two groups according to the length of their fur: short coated (called Ccara) and the medium coated (called Curaca).',
+    llamaSounds:
+      'Llamas make a variety of sounds. The most common sound is a humming noise. A female will hum to her cria (offspring). Males orgle, which sounds like a gurgle, during breeding. If a couple of males decide to have a fight, they will start screaming at each other. If a llama perceives danger, it sends an alarm call, which warns the rest of the herd.',
+    llamaAlarm:
+      'In the wild, a male will find a high vantage point to watch over his herd of females and if he spots danger, will start alarm calling. Moments later every male in the vicinity will be alarm calling.',
+    llamaSpit:
+      'The idea that llamas spit is true. Llamas usually spit to settle an argument over food or to decide which is the dominant llama. A female will also spit at a male to tell him to get lost. They do not normally spit at humans unless they are provoked. Their body language will warn other llamas; flattened ears are a signal to back off and are usually sufficient. The next threat may be a spitting sound, but using only air.',
+    llamaTamed:
+      'Also known as the "new world camel", llamas were domesticated in the early 1500s and are valuable work animals.',
+    llamaBreath:
+      'Llamas, like all camelids, differ from other mammals in that their red blood corpuscles are oval instead of round. This adaptation allows them to take in more oxygen, making them well suited to life at high altitudes.',
+    llamaPredators:
+      "In the wilds of South America, pumas (or mountain lions) are llamas' only natural predator. In North America, cougars and bears have been known to take llamas.",
+    llamaCarry: 'Llamas can carry 25% - 30% of their body weigh.',
+    llamaGrown: 'A llama becomes full grown at 4 years old.',
+    llamaLife: 'Llamas can live between 15 - 30 years.',
+    llamaFact: true
+  }
+});
