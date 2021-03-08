5ef29a8cc6d8749fcc75faa075587a4e56668cbb
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2021-03-07 21:52:53 -0600

    added media queries foor responsiveness to wk 7 assignment

--- wk7/assignment/main.js
+++ wk7/assignment/main.js
@@ -1,6 +1,7 @@
 var myPhones = [
   {
     brand: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.jimcdn.com%2Fapp%2Fcms%2Fimage%2Ftransf%2Fdimension%3D560x10000%3Aformat%3Dpng%2Fpath%2Fs4372e5f4ffcafa11%2Fimage%2Fi77c1334f47c826ed%2Fversion%2F1501151938%2Fblu-products-logo.png',
+    brandName: 'BLU',
     model: 'Tank 3',
     released: '2016',
     costThen: '25',
@@ -8,6 +9,7 @@ var myPhones = [
   },
   {
     brand: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2016%2F02%2FSamsung_logo_transparent-700x135.png',
+    brandName: 'Samsung',
     model: 'Impression',
     released: '2009',
     costThen: '399',
@@ -15,6 +17,7 @@ var myPhones = [
   },
   {
     brand: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fyourtechdose.files.wordpress.com%2F2014%2F09%2Fnokia.png',
+    brandName: 'Nokia',
     model: 'C6-01',
     released: '2016',
     costThen: '334',
@@ -22,6 +25,7 @@ var myPhones = [
   },
   {
     brand: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2016%2F02%2FSamsung_logo_transparent-700x135.png',
+    brandName: 'Samsung',
     model: 'Captivate Glide',
     released: '2011',
     costThen: '150',
@@ -29,6 +33,7 @@ var myPhones = [
   },
   {
     brand: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftokowarna.com%2Fwp-content%2Fuploads%2F2020%2F08%2FTokoWarna.com-Logo-Apple-PNG-Vector-CDR.png',
+    brandName: 'Apple',
     model: 'iPhone 4',
     released: '2011',
     costThen: '650',
