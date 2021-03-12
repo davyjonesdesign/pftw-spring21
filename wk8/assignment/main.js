26ffa36c8ae2999a4e8bcfbce0ffab46cee1c736
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2021-03-12 15:52:02 -0600

    adding 8.A

---
+++ wk8/assignment/main.js
@@ -0,0 +1,78 @@
+var myPhones = [
+  {
+    brand: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.jimcdn.com%2Fapp%2Fcms%2Fimage%2Ftransf%2Fdimension%3D560x10000%3Aformat%3Dpng%2Fpath%2Fs4372e5f4ffcafa11%2Fimage%2Fi77c1334f47c826ed%2Fversion%2F1501151938%2Fblu-products-logo.png',
+    brandName: 'BLU',
+    model: 'Tank 3',
+    released: '2016',
+    costThen: '25',
+    costNow: '20'
+  },
+  {
+    brand: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2016%2F02%2FSamsung_logo_transparent-700x135.png',
+    brandName: 'Samsung',
+    model: 'Impression',
+    released: '2009',
+    costThen: '399',
+    costNow: '39'
+  },
+  {
+    brand: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fyourtechdose.files.wordpress.com%2F2014%2F09%2Fnokia.png',
+    brandName: 'Nokia',
+    model: 'C6-01',
+    released: '2016',
+    costThen: '334',
+    costNow: '50'
+  },
+  {
+    brand: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2016%2F02%2FSamsung_logo_transparent-700x135.png',
+    brandName: 'Samsung',
+    model: 'Captivate Glide',
+    released: '2011',
+    costThen: '150',
+    costNow: '25'
+  },
+  {
+    brand: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftokowarna.com%2Fwp-content%2Fuploads%2F2020%2F08%2FTokoWarna.com-Logo-Apple-PNG-Vector-CDR.png',
+    brandName: 'Apple',
+    model: 'iPhone 4',
+    released: '2011',
+    costThen: '650',
+    costNow: '35'
+  }
+];
+
+var np = new Vue({
+  el: '#app',
+  data: {
+    newPhoneObj: {
+      brand: '',
+      brandName: '',
+      model: '',
+      released: null,
+      costThen: null,
+      costNow: null
+    },
+    phones: myPhones
+  },
+  methods: {
+    submitHandler: () => {
+      np.phones = np.phones.concat(np.newPhoneObj);
+      np.resetForm();
+    },
+    resetForm: () => {
+      np.newPhoneObj = {
+        brand: '',
+        brandName: '',
+        model: '',
+        released: null,
+        costThen: null,
+        costNow: null
+      };
+    },
+    deleteItem: item => {
+      np.phones = np.phones.filter(phone => {
+        return phone !== item;
+      })
+    }
+  }
+});
