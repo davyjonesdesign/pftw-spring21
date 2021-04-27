99278bfbf9125c6de1a00f3609a3f105ae9562c6
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2021-04-27 18:16:10 -0500

    adding wk 13 assignment

---
+++ wk13/assignment/particle.js
@@ -0,0 +1,64 @@
+var valueX;
+var valueY;
+function Particle () {
+  this.pos = createVector(random(width), random(height));
+  this.vel = createVector(0, 0);
+  this.acc = createVector(0, 0);
+  this.maxspeed = 3.5;
+
+  this.prevPos = this.pos.copy();
+
+  this.update = function () {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  };
+
+  this.follow = function (vectors) {
+    var x = floor(this.pos.x / scl);
+    var y = floor(this.pos.y / scl);
+    var index = x + y * cols;
+    var force = vectors[index];
+    this.applyForce(force);
+  };
+
+  this.applyForce = function (force) {
+    this.acc.add(force);
+  };
+
+  this.show = function () {
+    for (var i = 0; i < 3; i++) {
+      strokeWeight(1.5);
+      stroke(41, 31, 43, 15);
+      // Fill color according to mouseClicked()
+      stroke(random(20, 60), random(10, 50), random(20, 60), 15);
+    }
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    this.updatePrev();
+  };
+
+  this.updatePrev = function () {
+    this.prevPos.y = this.pos.y;
+    this.prevPos.x = this.pos.x;
+  };
+
+  this.edges = function () {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.updatePrev();
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.updatePrev();
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.updatePrev();
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.updatePrev();
+    }
+  };
+}
---
+++ wk13/assignment/sketch.js
@@ -0,0 +1,54 @@
+var inc = 0.1;
+var scl = 20;
+var cols, rows;
+
+var zoff = 0;
+
+var fr;
+
+var particles = [];
+
+var flowfield = [];
+
+function setup () {
+  createCanvas(800, 600, Path2D);
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  fr = createP();
+
+  flowfield = new Array(cols * rows);
+
+  for (var i = 0; i < 2000; i++) {
+    particles[i] = new Particle();
+  }
+
+  background(210, 161, 140);
+}
+
+function draw () {
+  var yoff = 0;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      var index = (x + y * cols);
+      flowfield[index] = v;
+      var angle = noise(xoff, yoff, zoff) * sin(inc) * 30;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      xoff += sin(inc);
+    }
+    yoff += sin(inc);
+    zoff += 0.0009;
+  }
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].edges();
+    particles[i].show();
+  }
+}
+
+function mousePressed () {
+  clear();
+  background(random(100, 210), random(50, 160), random(30, 140));
+}
